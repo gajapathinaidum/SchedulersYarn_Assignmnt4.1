8bdb2d4637cb863481fc9e4683ab9f23ebd0c2e8
gajapathinaidum <gajapathi.hdp@gmail.com>
2017-07-02 17:07:36 +0530

    Add files via upload

---
+++ Acad4.java
@@ -0,0 +1,12 @@
+package com.acadgild.mr.practice;
+
+public class Acad4 {
+
+	public static void multiplesOf(int a){
+		for(int i=1;i<=10;i++){
+		System.out.println(""+a+"*"+i+"="+(a*i));
+	}}
+	public static void main(String[] args) {
+		multiplesOf(3);
+	}
+}
