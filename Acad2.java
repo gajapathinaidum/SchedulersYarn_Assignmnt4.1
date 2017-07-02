1ffabe8dfe85926d3036d6ab1497807b9090dc1f
gajapathinaidum <gajapathi.hdp@gmail.com>
2017-07-02 16:57:16 +0530

    Add files via upload

---
+++ Acad2.java
@@ -0,0 +1,13 @@
+package com.acadgild.mr.practice;
+
+public class Acad2 {
+
+	public static void sum(int a,int b){
+		System.out.println("First Number is  "+a);
+		System.out.println("second Number is  "+b);
+		System.out.println("sum is  "+(a+b));
+	}
+	public static void main(String[] args) {
+		sum(30,20);
+	}
+}
